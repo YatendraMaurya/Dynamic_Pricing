5e512743a743023a069c6a72a433e7d990a31615
tushar <tushar.singh@grouponindia.in>
2018-07-04 17:05:01 +0530

    Added basic classes

--- src/main/java/com/nearbuy/dynamic/pricing/dynamicpricing/dao/BookingDao.java
+++ src/main/java/com/nearbuy/dynamic/pricing/dynamicpricing/dao/BookingDao.java
@@ -1,4 +1,12 @@
 package com.nearbuy.dynamic.pricing.dynamicpricing.dao;
 
+import com.nearbuy.dynamic.pricing.model.Booking;
+
+import java.util.Collection;
+
 public class BookingDao {
+
+    private Collection<Booking> bookingCollection;
+
+
 }
--- src/main/java/com/nearbuy/dynamic/pricing/dynamicpricing/util/AppRestClient.java
+++ src/main/java/com/nearbuy/dynamic/pricing/dynamicpricing/util/AppRestClient.java
@@ -2,11 +2,16 @@ package com.nearbuy.dynamic.pricing.dynamicpricing.util;
 
 import com.nearbuy.dynamic.pricing.dynamicpricing.service.model.AppRestRequest;
 import com.nearbuy.dynamic.pricing.dynamicpricing.service.model.AppRestResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * all services communications should happen through this class
  */
+@Component
 public class AppRestClient {
+
+    
 
     public <T> AppRestResponse<T> fireRequest(AppRestRequest req){
         return null;
