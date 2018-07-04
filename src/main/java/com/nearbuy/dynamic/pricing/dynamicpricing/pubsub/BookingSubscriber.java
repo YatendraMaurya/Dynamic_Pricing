bf9569d1471d466bddc5de2e4235a68e4ae369e7
tushar <tushar.singh@grouponindia.in>
2018-07-04 17:08:39 +0530

    Added sample code

--- src/main/java/com/nearbuy/dynamic/pricing/dynamicpricing/pubsub/BookingSubscriber.java
+++ src/main/java/com/nearbuy/dynamic/pricing/dynamicpricing/pubsub/BookingSubscriber.java
@@ -1,8 +1,24 @@
 package com.nearbuy.dynamic.pricing.dynamicpricing.pubsub;
 
+import com.nearbuy.dynamic.pricing.dynamicpricing.dao.BookingDao;
+import com.nearbuy.dynamic.pricing.dynamicpricing.service.BookingService;
+import com.nearbuy.dynamic.pricing.dynamicpricing.service.MerchantService;
 import com.nearbuy.dynamic.pricing.model.Booking;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookingSubscriber implements AppSubscriber<Booking>{
+
+    @Autowired
+    BookingService bookingService;
+
+    @Autowired
+    MerchantService merchantService;
+
+    @Autowired
+    BookingDao bookingDao;
+
 
     @Override
     public void consume(Booking o) {
