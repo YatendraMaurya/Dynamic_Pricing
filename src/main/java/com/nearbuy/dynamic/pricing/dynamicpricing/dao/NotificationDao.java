317c14f86fb430dc7d8357e9d6ddfc30538bb8bf
yatendramaurya <yatendra.maurya@nearbuy.com>
2018-07-24 15:49:25 +0530

    1->added some import statements 2->Changed Cashback from double array to double. 3->added slot and offerid(OptionId Field). 4->added optionId into notification model also. 5->added optionid in NotificationMongoModel 6->added slot,optionId and cashback in PalBookingModel 7->Slot and optionId added to BookingDao 8->modified hasNotififedRecently() method to consider OptionId also. 9->Integrate two other API's Inventory and DealService. 10->added some comments to BookingResponse and also change cashback from array to Double. 11->Deal Service and Inventory two new API's integrated. 12->NileDiscoveryPost request Body changed to include slot type also. 13->Notification Mongo model changed to include slot and optionType also 14->added new method tagsToSlot and slotToTags to map slot and tags.

--- src/main/java/com/nearbuy/dynamic/pricing/dynamicpricing/dao/NotificationDao.java
+++ src/main/java/com/nearbuy/dynamic/pricing/dynamicpricing/dao/NotificationDao.java
@@ -9,7 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-
+//sdc
 @Component
 public class NotificationDao {
     public static final Logger logger=LoggerFactory.getLogger(NotificationDao.class);
